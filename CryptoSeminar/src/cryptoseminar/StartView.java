2f5021a4fa46864a1392bd823f83417fab8b1361
Josip S <jskako@fesb.hr>
2020-01-21 14:34:47 +0100

    Edit
    Design edit, jtable added, arraylist

--- CryptoSeminar/src/cryptoseminar/StartView.java
+++ CryptoSeminar/src/cryptoseminar/StartView.java
@@ -5,14 +5,18 @@
  */
 package cryptoseminar;
 
+import java.awt.Component;
+import java.awt.event.KeyEvent;
 import java.sql.Connection;
 import java.sql.ResultSet;
-import java.awt.datatransfer.StringSelection;
-import java.awt.Toolkit;
-import java.awt.datatransfer.Clipboard;
 import java.net.URL;
-import java.util.List;
+import java.util.ArrayList;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumnModel;
 import wf.bitcoin.javabitcoindrpcclient.BitcoinJSONRPCClient;
+import wf.bitcoin.javabitcoindrpcclient.BitcoindRpcClient.Block;
 
 /**
  *
@@ -29,21 +33,33 @@ public class StartView extends javax.swing.JFrame {
     static final String password = "WYVyF5DTERJASAiIiYGg4UkRH";
     static final String host = "blockchain.oss.unist.hr";
     static final String port = "8332";
-
+    String connection;
+    URL url;
+    BitcoinJSONRPCClient client;
     String[] arraypk = null;
     String[] arrayjk = null;
-
     int userID = 0;
-
     //Spajanje na bazu
     ResultSet RS = null;
     ExecuteScriptsOnDatabase CALIzb = new ExecuteScriptsOnDatabase();
+    ArrayList<String> dataList = new ArrayList<String>();
 
     public StartView(Connection Conn, String Username) {
         this.conn = Conn;
         this.username = Username;
         initComponents();
         postaviUser();
+        spajanjeRPC();
+    }
+
+    private void spajanjeRPC() {
+        try {
+            connection = "http://" + user + ':' + password + "@" + host + ":" + port + "/";
+            url = new URL(connection.trim());
+            client = new BitcoinJSONRPCClient(url);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private void postaviUser() {
@@ -69,13 +85,19 @@ public class StartView extends javax.swing.JFrame {
         jInternalFrame1 = new javax.swing.JInternalFrame();
         jPanel1 = new javax.swing.JPanel();
         blockExplorerPanel = new javax.swing.JPanel();
-        blockButton = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        hashTable = new javax.swing.JTable();
         welcomePane = new javax.swing.JPanel();
         blockchainWelcome = new javax.swing.JLabel();
         exitLabel = new javax.swing.JLabel();
         welcomeLabelText = new javax.swing.JLabel();
         passwordChangeButton = new javax.swing.JButton();
         blockchainTextLabel = new javax.swing.JLabel();
+        heightTextField = new javax.swing.JTextField();
+        heightLabel = new javax.swing.JLabel();
+        searchButton = new javax.swing.JButton();
+        depthLabel = new javax.swing.JLabel();
+        depthTextField = new javax.swing.JTextField();
 
         jInternalFrame1.setVisible(true);
 
@@ -97,24 +119,35 @@ public class StartView extends javax.swing.JFrame {
 
         blockExplorerPanel.setBackground(new java.awt.Color(209, 209, 209));
 
-        blockButton.setText("ENTER");
-        blockButton.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                blockButtonActionPerformed(evt);
+        hashTable.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null}
+            },
+            new String [] {
+                "Hash"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
             }
         });
+        jScrollPane1.setViewportView(hashTable);
 
         javax.swing.GroupLayout blockExplorerPanelLayout = new javax.swing.GroupLayout(blockExplorerPanel);
         blockExplorerPanel.setLayout(blockExplorerPanelLayout);
         blockExplorerPanelLayout.setHorizontalGroup(
             blockExplorerPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(blockButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(blockExplorerPanelLayout.createSequentialGroup()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 417, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         blockExplorerPanelLayout.setVerticalGroup(
             blockExplorerPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, blockExplorerPanelLayout.createSequentialGroup()
-                .addContainerGap(373, Short.MAX_VALUE)
-                .addComponent(blockButton, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 438, Short.MAX_VALUE)
         );
 
         welcomePane.setBackground(new java.awt.Color(232, 232, 232));
@@ -148,7 +181,32 @@ public class StartView extends javax.swing.JFrame {
         blockchainTextLabel.setFont(new java.awt.Font("Segoe UI", 3, 24)); // NOI18N
         blockchainTextLabel.setForeground(new java.awt.Color(255, 255, 255));
         blockchainTextLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        blockchainTextLabel.setText("Blockchain Explorer");
+        blockchainTextLabel.setText("Blockchain Explorer History");
+
+        heightTextField.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                heightTextFieldKeyTyped(evt);
+            }
+        });
+
+        heightLabel.setFont(new java.awt.Font("Segoe UI", 2, 14)); // NOI18N
+        heightLabel.setText("Block height");
+
+        searchButton.setText("Search");
+        searchButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                searchButtonActionPerformed(evt);
+            }
+        });
+
+        depthLabel.setFont(new java.awt.Font("Segoe UI", 2, 14)); // NOI18N
+        depthLabel.setText("Block depth");
+
+        depthTextField.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                depthTextFieldKeyTyped(evt);
+            }
+        });
 
         javax.swing.GroupLayout welcomePaneLayout = new javax.swing.GroupLayout(welcomePane);
         welcomePane.setLayout(welcomePaneLayout);
@@ -156,17 +214,30 @@ public class StartView extends javax.swing.JFrame {
             welcomePaneLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(welcomePaneLayout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(blockchainWelcome)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(welcomePaneLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(blockchainTextLabel, javax.swing.GroupLayout.DEFAULT_SIZE, 695, Short.MAX_VALUE)
                     .addGroup(welcomePaneLayout.createSequentialGroup()
+                        .addGap(10, 10, 10)
+                        .addComponent(blockchainTextLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 404, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(heightLabel)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(heightTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(depthLabel)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(depthTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(searchButton, javax.swing.GroupLayout.PREFERRED_SIZE, 82, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(26, Short.MAX_VALUE))
+                    .addGroup(welcomePaneLayout.createSequentialGroup()
+                        .addComponent(blockchainWelcome)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(welcomePaneLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(welcomeLabelText)
                             .addComponent(passwordChangeButton))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(exitLabel)))
-                .addContainerGap())
+                        .addComponent(exitLabel)
+                        .addContainerGap())))
         );
         welcomePaneLayout.setVerticalGroup(
             welcomePaneLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -179,10 +250,17 @@ public class StartView extends javax.swing.JFrame {
                             .addComponent(welcomeLabelText))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(passwordChangeButton)
-                        .addGap(28, 28, 28)
-                        .addComponent(blockchainTextLabel))
+                        .addGap(64, 64, 64))
                     .addComponent(blockchainWelcome))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(welcomePaneLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(blockchainTextLabel)
+                    .addComponent(heightLabel)
+                    .addComponent(heightTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(depthLabel)
+                    .addComponent(depthTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(searchButton))
+                .addContainerGap())
         );
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
@@ -211,9 +289,7 @@ public class StartView extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGap(2, 2, 2))
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
@@ -232,30 +308,87 @@ public class StartView extends javax.swing.JFrame {
         CalPasC.setVisible(true);
     }//GEN-LAST:event_passwordChangeButtonActionPerformed
 
-    private void blockButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_blockButtonActionPerformed
+    private void searchButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_searchButtonActionPerformed
         try {
-            String connection = "http://" + user + ':' + password + "@" + host + ":" + port + "/";
-            System.out.println(connection);
-            URL url = new URL(connection.trim());
-            BitcoinJSONRPCClient client = new BitcoinJSONRPCClient(url);
-            List<String> rawMempool = client.getRawMemPool();
-            Double rawHex = client.getBalance("1KFHE7w8BhaENAswwryaoccDb6qcT6DbYY");
-            System.out.println(Integer.toString(rawMempool.size()));
+            if (heightTextField.getText().trim().equals("")) {
+                //Podigni Error
+                PopError CALError = new PopError();
+                CALError.infoBox("You entered wrong height!", "Error!");
+            } else {
+                //Očisti array
+                dataList.clear();
+                //Očisti listu
+                DefaultTableModel model = (DefaultTableModel) hashTable.getModel();
+                model.setRowCount(0);
+                Block myBlock = client.getBlock(Integer.parseInt(heightTextField.getText().trim()));
+                dataList.add(myBlock.hash());
+                int tmp = Integer.parseInt(depthTextField.getText().trim());
+                while (tmp > 0) {
+                    if (!myBlock.previousHash().equals("")) {
+                        dataList.add(myBlock.previousHash());
+                        myBlock = client.getBlock(myBlock.previousHash());
+                        tmp = tmp - 1;
+                    } else {
+                        break;
+                    }
+                }
+                ispisListe(model);
+            }
+            //System.out.println("Previous hash: " + myBlock.previousHash());
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }//GEN-LAST:event_blockButtonActionPerformed
+    }//GEN-LAST:event_searchButtonActionPerformed
 
+    private void ispisListe(DefaultTableModel model) {
+        for (int i = 0; i < dataList.size(); i++) {
+            String index = dataList.get(i);
+            Object[] data = {index};
+            model.addRow(data);
+        }
+    }
+
+    private void heightTextFieldKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_heightTextFieldKeyTyped
+        ZabraniSlova(evt);
+    }//GEN-LAST:event_heightTextFieldKeyTyped
+
+    private void depthTextFieldKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_depthTextFieldKeyTyped
+        ZabraniSlova(evt);
+    }//GEN-LAST:event_depthTextFieldKeyTyped
+
+    public void ZabraniBrojeve(java.awt.event.KeyEvent evt) {
+        char vChar = evt.getKeyChar();
+        if ((Character.isDigit(vChar)
+                || (vChar == KeyEvent.VK_BACK_SPACE)
+                || (vChar == KeyEvent.VK_DELETE))) {
+            evt.consume();
+        }
+    }
+
+    public void ZabraniSlova(java.awt.event.KeyEvent evt) {
+        char vChar = evt.getKeyChar();
+        if ((!Character.isDigit(vChar)
+                || (vChar == KeyEvent.VK_BACK_SPACE)
+                || (vChar == KeyEvent.VK_DELETE))) {
+            evt.consume();
+        }
+    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton blockButton;
     private javax.swing.JPanel blockExplorerPanel;
     private javax.swing.JLabel blockchainTextLabel;
     private javax.swing.JLabel blockchainWelcome;
+    private javax.swing.JLabel depthLabel;
+    private javax.swing.JTextField depthTextField;
     private javax.swing.JLabel exitLabel;
+    private javax.swing.JTable hashTable;
+    private javax.swing.JLabel heightLabel;
+    private javax.swing.JTextField heightTextField;
     private javax.swing.JInternalFrame jInternalFrame1;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JButton passwordChangeButton;
+    private javax.swing.JButton searchButton;
     private javax.swing.JLabel welcomeLabelText;
     private javax.swing.JPanel welcomePane;
     // End of variables declaration//GEN-END:variables
